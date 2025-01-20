f185c5ef0653de75fcf7acc116096e293c4a307e
tiedsandi <fachransandi@gmail.com>
2025-01-20 10:51:01 +0700

    1-adding dynamic styling, useState, rendering content conditionally

--- 1-react-essentials/src/App.jsx
+++ 1-react-essentials/src/App.jsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
 import CoreConcept from './components/CoreConcept';
 import Header from './components/Header/Header';
-import { CORE_CONCEPTS } from './data';
+import TabButton from './components/TabButton';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -19,6 +41,32 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={selectedTopic === 'components'}
+              onSelect={() => handleClick('components')}>
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'jsx'}
+              onSelect={() => handleClick('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'props'}
+              onSelect={() => handleClick('props')}>
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === 'state'}
+              onSelect={() => handleClick('state')}>
+              State
+            </TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
