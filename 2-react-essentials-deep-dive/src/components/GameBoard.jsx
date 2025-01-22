af03249201bff8e6af508b8686c05afc61ca34ca
tiedsandi <fachransandi@gmail.com>
2025-01-22 09:22:53 +0700

    2- multi-dimensional-lists and updating-state-immutably

--- 2-react-essentials-deep-dive/src/components/GameBoard.jsx
+++ 2-react-essentials-deep-dive/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialNameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,26 @@ const initialNameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialNameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialNameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
