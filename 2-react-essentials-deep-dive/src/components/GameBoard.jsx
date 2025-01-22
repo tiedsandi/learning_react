581622b1fa0edff71f1b28171b3f4d8c1723eb6d
tiedsandi <fachransandi@gmail.com>
2025-01-22 09:11:25 +0700

    2- creating gameBoard component

---
+++ 2-react-essentials-deep-dive/src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const initialNameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialNameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
