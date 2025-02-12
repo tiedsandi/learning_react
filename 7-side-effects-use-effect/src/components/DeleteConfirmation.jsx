d8c4d9f6504c19733f97518e65ffa143debd0f30
tiedsandi <fachransandi@gmail.com>
2025-02-12 07:34:04 +0700

    7-adding countdown in modal

--- 7-side-effects-use-effect/src/components/DeleteConfirmation.jsx
+++ 7-side-effects-use-effect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from 'react';
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [countdown, setCountdown] = useState(3);
+
+  useEffect(() => {
+    console.log('Timer set');
+
+    // const timer = setTimeout(() => {
+    //   onConfirm();
+    // }, 3000);
+
+    const interval = setInterval(() => {
+      setCountdown((prev) => {
+        if (prev <= 1) {
+          clearInterval(interval);
+          onConfirm();
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+      // clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -8,7 +36,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           No
         </button>
         <button onClick={onConfirm} className="button">
-          Yes
+          Yes {countdown} seconds
         </button>
       </div>
     </div>
