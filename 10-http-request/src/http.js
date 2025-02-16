29ebdbb4602c4340cc323144ca93dcb7340284eb
tiedsandi <fachransandi@gmail.com>
2025-02-17 04:46:53 +0700

    10-fixing bug post

--- 10-http-request/src/http.js
+++ 10-http-request/src/http.js
@@ -11,7 +11,7 @@ export async function fetchAvailablePlaces() {
 }
 
 export async function updateUserPlaces(places) {
-  const response = fetch('http://localhost:3000/user-places', {
+  const response = await fetch('http://localhost:3000/user-places', {
     method: 'PUT',
     body: JSON.stringify({ places }),
     headers: {
