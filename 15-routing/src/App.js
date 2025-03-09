59c9c19ca234e654d8d05b767be4ecb691c29685
tiedsandi <fachransandi@gmail.com>
2025-03-09 17:17:18 +0700

    15- layouts & nested routes

--- 15-routing/src/App.js
+++ 15-routing/src/App.js
@@ -7,6 +7,7 @@ import {
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -14,14 +15,18 @@ import ProductsPage from './pages/Products';
 //     <Route path="/products" element={<ProductsPage />} />
 //   </Route>
 // );
+// const router = createBrowserRouter(routeDefintions);
 
-const router = createBrowserRouter(
-  // routeDefintions
-  [
-    { path: '/', element: <HomePage /> },
-    { path: '/products', element: <ProductsPage /> },
-  ]
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
