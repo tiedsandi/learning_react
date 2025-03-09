6ac48dd951f387360f6fff82a3b9888827303c96
tiedsandi <fachransandi@gmail.com>
2025-03-09 17:04:44 +0700

    15- navigation between pages with links

--- 15-routing/src/App.js
+++ 15-routing/src/App.js
@@ -1,21 +1,27 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
+  // createRoutesFromElements,
   RouterProvider,
-  Route,
+  // Route,
 } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
-const router = createBrowserRouter(routeDefinitions);
+const router = createBrowserRouter(
+  // routeDefintions
+  [
+    { path: '/', element: <HomePage /> },
+    { path: '/products', element: <ProductsPage /> },
+  ]
+);
 
 function App() {
   return <RouterProvider router={router} />;
--- 15-routing/src/pages/Products.js
+++ 15-routing/src/pages/Products.js
@@ -1,3 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function ProductsPage() {
-  return <h1>Product page</h1>;
+  return (
+    <>
+      <h1>Product page</h1>
+      <p>
+        Go to <Link to="/">Home</Link>
+      </p>
+    </>
+  );
 }
