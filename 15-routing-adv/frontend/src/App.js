0c4fb1cd24e281e9ba5f97293e347b6af8278298
tiedsandi <fachransandi@gmail.com>
2025-03-13 09:27:08 +0700

    15b- datafetching with loader

--- 15-routing-adv/frontend/src/App.js
+++ 15-routing-adv/frontend/src/App.js
@@ -40,7 +40,20 @@ const router = createBrowserRouter([
         path: 'events',
         element: <EventsLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch('http://localhost:8080/events');
+
+              if (!response.ok) {
+                // ...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: ':eventid', element: <EventDetailPage /> },
           { path: 'new', element: <NewEventPage /> },
           { path: ':eventid/edit', element: <EditEventPage /> },
