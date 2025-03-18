23a5f4d220175326d7288171fa9e842b6badbb58
tiedsandi <fachransandi@gmail.com>
2025-03-18 07:41:03 +0700

    15b- useFetcher()

--- 15-routing-adv/frontend/src/components/MainNavigation.js
+++ 15-routing-adv/frontend/src/components/MainNavigation.js
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
+import NewsletterSignup from './NewsletterSignup';
 
 function MainNavigation() {
   return (
@@ -9,7 +10,7 @@ function MainNavigation() {
         <ul className={classes.list}>
           <li>
             <NavLink
-              to={'/'}
+              to="/"
               className={({ isActive }) => (isActive ? classes.active : undefined)}
               end>
               Home
@@ -17,13 +18,21 @@ function MainNavigation() {
           </li>
           <li>
             <NavLink
-              to={'/events'}
+              to="/events"
               className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Events
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) => (isActive ? classes.active : undefined)}>
+              Newsletter
+            </NavLink>
+          </li>
         </ul>
       </nav>
+      <NewsletterSignup />
     </header>
   );
 }
