1426e51776c5fed7dfd112602caa1cffd1244a96
tiedsandi <fachransandi@gmail.com>
2025-05-03 14:20:47 +0700

    16- query for login or signup form using useSearchParams

---
+++ 16-authentication/README.md
@@ -0,0 +1,5 @@
+# AUTHETICATION
+
+## Apa itu Auth?
+
+## Query untuk switch login atau signup (useSearchParams from react-router-dom)
--- 16-authentication/frontend/src/components/MainNavigation.js
+++ 16-authentication/frontend/src/components/MainNavigation.js
@@ -1,7 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
-import NewsletterSignup from './NewsletterSignup';
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
   return (
@@ -11,32 +11,30 @@ function MainNavigation() {
           <li>
             <NavLink
               to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
+              className={({ isActive }) => (isActive ? classes.active : undefined)}
+              end>
               Home
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/events"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
+              className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Events
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/newsletter"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
+              className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Newsletter
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/auth"
+              className={({ isActive }) => (isActive ? classes.active : undefined)}>
+              Authetication
             </NavLink>
           </li>
         </ul>
