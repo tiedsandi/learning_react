e2f9d2eb84b61266a8969abafd70bd1cb061e8b1
tiedsandi <fachransandi@gmail.com>
2025-05-03 16:36:15 +0700

    16- adding user-logout

--- 16-authentication/frontend/src/components/MainNavigation.js
+++ 16-authentication/frontend/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Form, NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 import NewsletterSignup from "./NewsletterSignup";
@@ -36,6 +36,11 @@ function MainNavigation() {
               className={({ isActive }) => (isActive ? classes.active : undefined)}>
               Authetication
             </NavLink>
+          </li>
+          <li>
+            <Form action="/logout" method="post">
+              <button>Logout</button>
+            </Form>
           </li>
         </ul>
       </nav>
