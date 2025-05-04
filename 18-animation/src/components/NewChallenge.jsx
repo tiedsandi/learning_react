a199138de4946d806a16738956f9be94249b0d03
tiedsandi <fachransandi@gmail.com>
2025-05-04 14:54:55 +0700

    18- animating colors and working with keyframes

--- 18-animation/src/components/NewChallenge.jsx
+++ 18-animation/src/components/NewChallenge.jsx
@@ -66,10 +66,10 @@ export default function NewChallenge({ onDone }) {
             <motion.li
               variants={{
                 hidden: { opacity: 0, scale: 0.5 },
-                visible: { opacity: 1, scale: 1 },
+                visible: { opacity: 1, scale: [0.8, 1.3, 1] },
               }}
               // exit={{ opacity: 1, scale: 1 }}
-              transition={{ type: "spring" }}
+              transition={{ type: "tween", duration: 0.5 }}
               key={image.alt}
               onClick={() => handleSelectImage(image)}
               className={selectedImage === image ? "selected" : undefined}>
