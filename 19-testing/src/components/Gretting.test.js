0f2eab624d9d2559a2a97e120bf12f21c5946508
tiedsandi <fachransandi@gmail.com>
2025-05-05 10:19:07 +0700

    19- writng first test

---
+++ 19-testing/src/components/Gretting.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+
+import Greeting from "./Greeting";
+
+test("renders Hello World as a text", () => {
+  // Arrage
+  render(<Greeting />);
+
+  // Act
+  // Nothing...
+
+  // Assert
+  const helloWorldElement = screen.getByText("Hello World!");
+  expect(helloWorldElement).toBeInTheDocument();
+});
